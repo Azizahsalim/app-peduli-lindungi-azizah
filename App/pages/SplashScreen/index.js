c9dd701c5b0d028a03ef006e2acd054861a37c2b
Azizahsalim <salazizah21@gmail.com>
2022-10-13 12:47:05 +0700

    edit

--- App/pages/SplashScreen/index.js
+++ App/pages/SplashScreen/index.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     height: 30,
     left: 38,
     right: 228,
-    top: 670,
+    top: 650,
     bottom: 131,
     fontFamily: "Lato-Regular",
     fontStyle: "normal",
